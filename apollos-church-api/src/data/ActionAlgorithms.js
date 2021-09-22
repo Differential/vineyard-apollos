f4c87a8c3d005e484cd51204073904ad35c2b50b
nlewis84 <nathan@differential.com>
2021-09-22 15:36:51 -0500

    moved the custom nature of this mostly to config.postgres.yml

--- apollos-church-api/src/data/ActionAlgorithms.js
+++ apollos-church-api/src/data/ActionAlgorithms.js
@@ -3,32 +3,14 @@ import { ActionAlgorithm } from '@apollosproject/data-connector-rock';
 class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
-    BE_READY_CONTENT_FEED: this.beReadyContentFeedAlgorithm.bind(this),
-    GET_SET_CONTENT_FEED: this.getSetContentFeedAlgorithm.bind(this),
-    GO_SERVE_CONTENT_FEED: this.goServeContentFeedAlgorithm.bind(this),
   };
 
-  async contentFeedAlgorithm({ channelIds = [], limit = 20, skip = 0 } = {}) {
-    const { ContentItem } = this.context.dataSources;
-    const items = await ContentItem.getFromCategoryIds(channelIds, {
-      limit,
-      skip,
-    });
-    return items.map((item, i) => ({
-      id: `${item.id}${i}`,
-      title: item.title,
-      subtitle: item.contentChannel?.name,
-      relatedNode: item,
-      image: item.getCoverImage(),
-      action: 'READ_CONTENT',
-      summary: item.summary,
-    }));
-  }
-
-  async beReadyContentFeedAlgorithm({
+  async contentFeedAlgorithm({
+    subtitle = '',
     channelIds = [],
-    limit = 1,
+    limit = 20,
     skip = 0,
+    hasImage = true,
   } = {}) {
     const { ContentItem } = this.context.dataSources;
     const items = await ContentItem.getFromCategoryIds(channelIds, {
@@ -38,51 +20,9 @@ class dataSource extends ActionAlgorithm.dataSource {
     return items.map((item, i) => ({
       id: `${item.id}${i}`,
       title: item.title,
-      subtitle: 'Be Ready',
+      subtitle: subtitle || item.contentChannel?.name,
       relatedNode: item,
-      image: null,
-      action: 'READ_CONTENT',
-      summary: item.summary,
-    }));
-  }
-
-  async getSetContentFeedAlgorithm({
-    channelIds = [],
-    limit = 1,
-    skip = 0,
-  } = {}) {
-    const { ContentItem } = this.context.dataSources;
-    const items = await ContentItem.getFromCategoryIds(channelIds, {
-      limit,
-      skip,
-    });
-    return items.map((item, i) => ({
-      id: `${item.id}${i}`,
-      title: item.title,
-      subtitle: 'Get Set',
-      relatedNode: item,
-      image: null,
-      action: 'READ_CONTENT',
-      summary: item.summary,
-    }));
-  }
-
-  async goServeContentFeedAlgorithm({
-    channelIds = [],
-    limit = 1,
-    skip = 0,
-  } = {}) {
-    const { ContentItem } = this.context.dataSources;
-    const items = await ContentItem.getFromCategoryIds(channelIds, {
-      limit,
-      skip,
-    });
-    return items.map((item, i) => ({
-      id: `${item.id}${i}`,
-      title: 'Find Your Next Service',
-      subtitle: 'Go Serve',
-      relatedNode: item,
-      image: null,
+      image: hasImage ? item.getCoverImage() : null,
       action: 'READ_CONTENT',
       summary: item.summary,
     }));

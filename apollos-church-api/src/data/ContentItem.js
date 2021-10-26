e2a8935bc0be65d9ecd2c356ea09fb98b1f10018
nlewis84 <nathan@differential.com>
2021-10-26 15:45:01 -0500

    removed old code from content item

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -16,36 +16,6 @@ class dataSource extends ContentItem.dataSource {
         !['EventDateFeature', 'LocationFeature'].includes(feature.apollosType)
     );
   };
-
-  getFromCategoryIds = (ids = [], args = {}) => {
-    if (ids.some((id) => typeof id === 'number')) {
-      console.warn(
-        'You are passing rock ids IDS to ContentItem.getFromCategoryIds. This is supported, but we recommend using Postgres IDS in your config.yml long term'
-      );
-      return this.model.findAll({
-        ...args,
-        include: [
-          {
-            model: this.sequelize.models.contentItemCategory,
-            where: {
-              originId: { [Op.in]: ids.map(String) },
-              originType: 'rock',
-            },
-          },
-          ...(args?.include || []),
-        ],
-      });
-    }
-    return this.model.findAll({
-      ...args,
-      where: {
-        contentItemCategoryId: { [Op.in]: ids },
-        ...args?.where,
-      },
-      // This is custom....normally it is sorted by DESC
-      order: [['publishAt', 'ASC']],
-    });
-  };
 }
 
 export { schema, resolver, dataSource, models, migrations };

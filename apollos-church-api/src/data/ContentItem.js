155b385c2674244f51470ff2f55bdcf5e2c90af3
nlewis84 <nathan@differential.com>
2021-11-30 15:41:12 -0600

    chore: update packages and start of work adding in action table on get set items

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -4,15 +4,41 @@ const { schema, resolver, models, migrations } = ContentItem;
 
 class dataSource extends ContentItem.dataSource {
   getFeatures = async (model) => {
+    const { Feature } = this.context.dataSources;
     const features = await model.getFeatures({
       order: [['priority', 'ASC']],
     });
+
     // filter out location and event features
     // those are handled differently and are shown above the content
-    return features.filter(
+    const filteredFeatures = features.filter(
       (feature) =>
         !['EventDateFeature', 'LocationFeature'].includes(feature.apollosType)
     );
+
+    if (
+      model.dataValues.contentItemCategoryId ===
+      '1acff1cb-805b-4561-b5f7-ee136b0b39b4'
+    ) {
+      filteredFeatures.push(
+        Feature.createActionTableFeature({
+          __typename: 'ActionTableFeature',
+          title: 'test',
+          actions: [
+            {
+              title: 'Share your story',
+              action: 'OPEN_URL',
+              relatedNode: {
+                __typename: 'Url',
+                url: 'GoDoGood://app-link/nav/Stories',
+              },
+            },
+          ],
+        })
+      );
+    }
+
+    return filteredFeatures;
   };
 }
 
